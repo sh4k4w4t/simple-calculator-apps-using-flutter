6c414e2be5e2d4c3ed7df45e0c29124cd8aefded
Bengal PC <shakawat_hossain@outlook.com>
2023-03-02 16:18:30 +0600

    Simple Calculator Apps Using Dart & Flutter

---
+++ android/app/src/main/java/sh4k4w4t/github/io/simple_calculator_app_with_flutter/simple_calculator_app_with_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package sh4k4w4t.github.io.simple_calculator_app_with_flutter.simple_calculator_app_with_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
